6c5453368402ef1953d2038b51edfea48b5b4525
sedtyldz <ikinciyargic@gmail.com>
2024-06-23 16:21:41 +0300

    DENEME SEDAT

--- test.via/ContentView.swift
+++ test.via/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!Deneme")
+            Text("Hello, world!Deneme3")
         }
         .padding()
     }
