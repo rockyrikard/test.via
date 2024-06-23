1840c5fd3e575940b3bf845211830fe384640662
Zafer Aydin <zafer.aydin.001@gmail.com>
2024-06-23 15:52:32 +0300

    Initial Commit

---
+++ test.via/test_viaApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_viaApp.swift
+//  test.via
+//
+//  Created by Zafer Aydın on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_viaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
